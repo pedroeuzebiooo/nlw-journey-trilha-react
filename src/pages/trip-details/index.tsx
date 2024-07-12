76d526ac39bf5774936b18dd7e0d36b6e5cf1270
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-07-12 14:46:37 -0300

    feat: add activities title trip details page

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,4 +1,4 @@
-import { Calendar, MapPin, Settings2 } from "lucide-react";
+import { Calendar, MapPin, Plus, Settings2 } from "lucide-react";
 
 export function TripDetailsPage() {
   return (
@@ -23,6 +23,19 @@ export function TripDetailsPage() {
           </button>
         </div>
       </div>
+
+      <main className="flex gap-16 px-4">
+        <div className="flex-1 space-y-6">
+          <div className="flex items-center justify-between">
+            <h2 className="text-3xl font-semibold">Atividades</h2>
+
+            <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+              <Plus className="size-5" />
+              Cadastrar atividade
+            </button>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
