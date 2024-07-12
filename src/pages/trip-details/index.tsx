b1f5948dc022668843ca8af932eddbb39cf513f3
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-07-12 14:51:48 -0300

    feat: add details peope trip details page

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,10 +1,12 @@
 import {
   Calendar,
   CircleCheck,
+  CircleDashed,
   Link2,
   MapPin,
   Plus,
   Settings2,
+  UserCog,
 } from "lucide-react";
 
 export function TripDetailsPage() {
@@ -136,6 +138,43 @@ export function TripDetailsPage() {
           </div>
 
           <div className="w-full h-px bg-zinc-800"></div>
+
+          <div className="space-y-6">
+            <h2 className="font-semibold text-xl">Convidados</h2>
+
+            <div className="space-y-5">
+              <div className="flex items-center justify-between gap-4">
+                <div className="space-y-1.5">
+                  <span className="block font-medium text-zinc-100">
+                    Jessica White
+                  </span>
+                  <span className="block text-sm text-zinc-400 truncate">
+                    jessica.white44@yahoo.com
+                  </span>
+                </div>
+
+                <CircleDashed className="size-5 text-zinc-400 shrink-0" />
+              </div>
+
+              <div className="flex items-center justify-between gap-4">
+                <div className="space-y-1.5">
+                  <span className="block font-medium text-zinc-100">
+                    Jessica White
+                  </span>
+                  <span className="block text-sm text-zinc-400 truncate">
+                    jessica.white44@yahoo.com
+                  </span>
+                </div>
+
+                <CircleDashed className="size-5 text-zinc-400 shrink-0" />
+              </div>
+            </div>
+
+            <button className="bg-zinc-800 text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700 w-full justify-center">
+              <UserCog className="size-5" />
+              Gerenciar convidados
+            </button>
+          </div>
         </div>
       </main>
     </div>
