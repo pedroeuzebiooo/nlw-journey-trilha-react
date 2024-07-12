bc0f97d9e0abb1206c52a8c81c2c9cf469cf86bd
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-07-11 23:44:08 -0300

    feat: add content

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { MapPin, Calendar, ArrowRight } from "lucide-react";
+
 export function App() {
   return (
     <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
@@ -7,6 +9,33 @@ export function App() {
           <p className="text-zinc-300 text-lg">
             Convide seus amigos e planeje sua próxima viagem!
           </p>
+        </div>
+
+        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+          <div className="flex items-center gap-2 flex-1">
+            <MapPin className="size-5 text-zinc-400" />
+            <input
+              type="text"
+              placeholder="Para onde você vai?"
+              className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+            />
+          </div>
+
+          <div className="flex items-center gap-2">
+            <Calendar className="size-5 text-zinc-400" />
+            <input
+              type="text"
+              placeholder="Quando?"
+              className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none"
+            />
+          </div>
+
+          <div className="w-px h-6 bg-zinc-800" />
+
+          <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+            Continuar
+            <ArrowRight className="size-5" />
+          </button>
         </div>
       </div>
     </div>
