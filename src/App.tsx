409d820b11ef483aff0ec7e778aa36a890d6f597
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-07-11 23:42:51 -0300

    feat: add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 export function App() {
   return (
-    <h1 className="text-3xl font-bold underline cursor-pointer">Hello World</h1>
+    <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
+      <div className="w-full max-w-3xl px-6 text-center space-y-10">
+        <div className="flex flex-col items-center gap-3">
+          <img src="/logo.svg" alt="plann.er" />
+          <p className="text-zinc-300 text-lg">
+            Convide seus amigos e planeje sua próxima viagem!
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
