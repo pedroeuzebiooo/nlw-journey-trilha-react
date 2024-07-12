8ff3a92687920872251c82757a51dfd8ec47108e
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-07-12 14:49:51 -0300

    feat: add details links trip details page

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,4 +1,11 @@
-import { Calendar, CircleCheck, MapPin, Plus, Settings2 } from "lucide-react";
+import {
+  Calendar,
+  CircleCheck,
+  Link2,
+  MapPin,
+  Plus,
+  Settings2,
+} from "lucide-react";
 
 export function TripDetailsPage() {
   return (
@@ -81,6 +88,51 @@ export function TripDetailsPage() {
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+
+        <div className="w-80 space-y-6">
+          <div className="space-y-6">
+            <h2 className="font-semibold text-xl">Links importantes</h2>
+
+            <div className="space-y-5">
+              <div className="flex items-center justify-between gap-4">
+                <div className="space-y-1.5">
+                  <span className="block font-medium text-zinc-100">
+                    Reserva do AirBnB
+                  </span>
+                  <a
+                    href=""
+                    className="block text-xs text-zinc-400 truncate hover:text-zinc-200"
+                  >
+                    https://www.airbnb.com.br/rooms/10470001123456789098765432
+                  </a>
+                </div>
+
+                <Link2 className="size-5 text-zinc-400 shrink-0" />
+              </div>
+
+              <div className="flex items-center justify-between gap-4">
+                <div className="space-y-1.5">
+                  <span className="block font-medium text-zinc-100">
+                    Reserva do AirBnB
+                  </span>
+                  <a
+                    href=""
+                    className="block text-xs text-zinc-400 truncate hover:text-zinc-200"
+                  >
+                    https://www.airbnb.com.br/rooms/10470001123456789098765432
+                  </a>
+                </div>
+
+                <Link2 className="size-5 text-zinc-400 shrink-0" />
+              </div>
+            </div>
+
+            <button className="bg-zinc-800 text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700 w-full justify-center">
+              <Plus className="size-5" />
+              Cadastrar novo link
+            </button>
           </div>
         </div>
       </main>
