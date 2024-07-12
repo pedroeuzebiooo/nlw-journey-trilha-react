fe8766deb53d3b5344ef87d33250b293dabbc040
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-07-12 18:00:26 -0300

    feat: add guests component

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,15 +1,8 @@
-import {
-  Calendar,
-  CircleCheck,
-  CircleDashed,
-  MapPin,
-  Plus,
-  Settings2,
-  UserCog,
-} from "lucide-react";
+import { Calendar, CircleCheck, MapPin, Plus, Settings2 } from "lucide-react";
 import { useState } from "react";
 import { CreateActivityModal } from "./create-activity-modal";
 import { ImportantLinks } from "./important-links";
+import { Guests } from "./guests";
 
 export function TripDetailsPage() {
   const [isCreateActivityModalOpen, setIsCreateActivityModalOpen] =
@@ -114,42 +107,7 @@ export function TripDetailsPage() {
 
           <div className="w-full h-px bg-zinc-800"></div>
 
-          <div className="space-y-6">
-            <h2 className="font-semibold text-xl">Convidados</h2>
-
-            <div className="space-y-5">
-              <div className="flex items-center justify-between gap-4">
-                <div className="space-y-1.5">
-                  <span className="block font-medium text-zinc-100">
-                    Jessica White
-                  </span>
-                  <span className="block text-sm text-zinc-400 truncate">
-                    jessica.white44@yahoo.com
-                  </span>
-                </div>
-
-                <CircleDashed className="size-5 text-zinc-400 shrink-0" />
-              </div>
-
-              <div className="flex items-center justify-between gap-4">
-                <div className="space-y-1.5">
-                  <span className="block font-medium text-zinc-100">
-                    Jessica White
-                  </span>
-                  <span className="block text-sm text-zinc-400 truncate">
-                    jessica.white44@yahoo.com
-                  </span>
-                </div>
-
-                <CircleDashed className="size-5 text-zinc-400 shrink-0" />
-              </div>
-            </div>
-
-            <button className="bg-zinc-800 text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700 w-full justify-center">
-              <UserCog className="size-5" />
-              Gerenciar convidados
-            </button>
-          </div>
+          <Guests />
         </div>
       </main>
 
