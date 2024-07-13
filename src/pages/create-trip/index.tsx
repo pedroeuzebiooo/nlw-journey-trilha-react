1d1bdd9add5d9097d415ede4e694d2a7ce6d83df
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-07-13 00:59:17 -0300

    style: adjust width create trip page

--- src/pages/create-trip/index.tsx
+++ src/pages/create-trip/index.tsx
@@ -117,7 +117,7 @@ export function CreateTripPage() {
 
   return (
     <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
-      <div className="w-full max-w-3xl px-6 text-center space-y-10">
+      <div className="w-full max-w-5xl px-6 text-center space-y-10">
         <div className="flex flex-col items-center gap-3">
           <img src="/logo.svg" alt="plann.er" />
           <p className="text-zinc-300 text-lg">
